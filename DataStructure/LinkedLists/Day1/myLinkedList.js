d29226bce9667ade71665c92540e3b574ba41631
Ankit Chaudhary <ankit.chaudhary@tietoevry.com>
2022-01-31 22:33:03 +0530

    commited

--- DataStructure/LinkedLists/Day1/myLinkedList.js
+++ DataStructure/LinkedLists/Day1/myLinkedList.js
@@ -1,4 +1,4 @@
-class MyObj {
+class Node{
     constructor(value) {
         this.value = value;
         this.next = null;
@@ -17,7 +17,7 @@ class MyLinkedList{
     }
 
     append(value){
-        const newNode = new MyObj(value);
+        const newNode = new Node(value);
         this.tail.next = newNode;
         this.tail = newNode;
         this.length++;
@@ -25,11 +25,56 @@ class MyLinkedList{
     }
 
     prepend(value){
-        const newNode = new MyObj(value);
+        const newNode = new Node(value);
         newNode.next = this.head;
         this.head = newNode;
         this.length++;
         return this;
+    }
+
+    insert(index, value){
+
+        if(index >= this.length){
+           return this.append(value);
+        }
+        const newNode = new Node(value);
+        const leaderNode = this.traverse(index-1);
+        const leaderNextNode = leaderNode.next;
+        leaderNode.next = newNode;
+        newNode.next = leaderNextNode;
+        this.length++;
+    }
+
+    remove(index){
+
+        if(index > this.length  || index < 0){
+            return "invalid index";
+        }
+
+        let headNode = this.traverse(index - 1);
+        let unwantedNode = headNode.next;
+        headNode.next=unwantedNode.next;
+        this.length--;
+    }
+
+    traverse(index){
+        let currentNode = this.head;
+        let counter = 0;
+        while(counter != index){
+            currentNode = currentNode.next;
+            counter++;
+        }
+        return currentNode;
+    }
+
+    printLL(){
+        const array = [];
+        let currentNode = this.head;
+        while(currentNode != null){
+            array.push(currentNode.value);
+            currentNode= currentNode.next;
+        }
+        return array;
     }
 }
 
@@ -37,6 +82,14 @@ const myLinkedList= new MyLinkedList(10);
 myLinkedList.append(20);
 myLinkedList.append(30);
 myLinkedList.append(40);
-myLinkedList.prepend(5);
+myLinkedList.prepend(5);//0
 myLinkedList.append(50);
+myLinkedList.insert(1,1);//1
+myLinkedList.insert(20,1);
+myLinkedList.remove(1);
+myLinkedList.append(60);
+myLinkedList.remove(1);
+console.log(myLinkedList.remove(20));
+console.log(myLinkedList.printLL());
+// console.log(myLinkedList.traverse(1));
 console.log(myLinkedList);
